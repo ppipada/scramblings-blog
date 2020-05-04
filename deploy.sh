380532fb07ced73cbc2560640a12d0322e538ed1
pankajp <pankaj.pipada@druva.com>
2020-05-04 17:51:58 +0530

    move to hugo

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "Deploying updates to GitHub...\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
