2b3efe06257f8237962b982339422e3caf7fca3b
pankajp <pankaj.pipada@druva.com>
2020-05-05 16:21:24 +0530

    add single page layout with toc

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,24 @@ printf "Deploying updates to GitHub...\n"
 # Build the project.
 hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
+# Go To Public folder i.e submodule
 cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# push commit to origin repo too.
+cd ..
 
 # Add changes to git.
 git add .
